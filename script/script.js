aeb32ef1c0e6e0396ece839015df5d034052b29c
Amuthakandasamy <amudha.mail@gmail.com>
2025-09-06 15:46:34 +0530

    practice loops in js

---
+++ script/script.js
@@ -0,0 +1,27 @@
+function add(a,b){
+return{
+    SUM : a+b,
+    DIFF : a-b,
+    MUL :  a*b,
+    DIV : a/b,
+    REM : a%b
+    }
+}
+console.log(add(12,4));
+
+
+
+/*
+1. print the numbers from 1 to 100 with the interval of 10
+2. print the number from 10 to 1
+3. print the multiple of 2 until 10
+4. print the number from 100 to 1 with the difference of 10.
+5. write a function to print the eligble, Not eligble voters separately from the list
+of array [18,17,16,15,10,8,7,9,35,40,86,75,0]. Note: print invalid if the voter age is 0.
+*/
+
+
+for (let i=1;i<=100;i++)
+{
+console.log(i);
+}
